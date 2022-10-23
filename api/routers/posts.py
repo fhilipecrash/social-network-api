a4019b1ec4233b99f4d92aac8bdfcc1f554c3f6e
Fhilipe Coelho <fhilipecoelho.dev@gmail.com>
2022-10-23 16:43:43 -0300

    `refactor`: change crud module to controller module

--- api/routers/posts.py
+++ api/routers/posts.py
@@ -1,7 +1,7 @@
 from api.database import get_db
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from api import schemas, crud
+from api import schemas, controllers
 
 router = APIRouter(
     prefix="/posts",
@@ -12,12 +12,12 @@ router = APIRouter(
 
 @router.get("", response_model=list[schemas.PostWithUserId])
 def get_posts(db: Session = Depends(get_db)):
-    return crud.get_posts(db)
+    return controllers.get_posts(db)
 
 
 @router.get("/{post_id}", response_model=schemas.PostWithUserId)
 def get_post(post_id: int, db: Session = Depends(get_db)):
-    db_post = crud.get_post(db, post_id)
+    db_post = controllers.get_post(db, post_id)
     if not db_post:
         raise HTTPException(status_code=404, detail="Post not found")
     return db_post
@@ -25,20 +25,20 @@ def get_post(post_id: int, db: Session = Depends(get_db)):
 
 @router.post("/create", response_model=schemas.PostWithUserId, status_code=201)
 def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)):
-    return crud.create_user_post(db, post)
+    return controllers.create_user_post(db, post)
 
 
 @router.put("/update/{post_id}", response_model=schemas.PostWithUserId)
 def update_post(post: schemas.PostBase, post_id: int, db: Session = Depends(get_db)):
-    db_post = crud.get_post(db, post_id)
+    db_post = controllers.get_post(db, post_id)
     if not db_post:
         raise HTTPException(status_code=404, detail="Post not found")
-    return crud.update_post(db, post, post_id)
+    return controllers.update_post(db, post, post_id)
 
 
 @router.delete("/delete/{post_id}", status_code=204)
 def delete_post(post_id: int, db: Session = Depends(get_db)):
-    db_post = crud.get_post(db, post_id)
+    db_post = controllers.get_post(db, post_id)
     if not db_post:
         raise HTTPException(status_code=404, detail="Post not found")
-    crud.delete_post(db, post_id)
+    controllers.delete_post(db, post_id)
--- api/routers/users.py
+++ api/routers/users.py
@@ -1,7 +1,7 @@
 from api.database import get_db
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from api import schemas, crud
+from api import schemas, controllers
 
 router = APIRouter(
     prefix="/users",
@@ -13,16 +13,16 @@ router = APIRouter(
 @router.get("", response_model=list[schemas.User] | schemas.User)
 def get_users(email: str | None = None, db: Session = Depends(get_db)):
     if email:
-        db_user = crud.get_user_by_email(db, email)
+        db_user = controllers.get_user_by_email(db, email)
         if not db_user:
             raise HTTPException(status_code=404, detail="User not found")
         return db_user
-    return crud.get_users(db)
+    return controllers.get_users(db)
 
 
 @router.get("/{user_id}", response_model=schemas.User)
 def get_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id)
+    db_user = controllers.get_user(db, user_id)
     if not db_user:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
@@ -31,11 +31,11 @@ def get_user(user_id: int, db: Session = Depends(get_db)):
 @router.get("/{user_id}/posts", response_model=list[schemas.PostWithUserId] | schemas.UserWithPosts)
 def get_user_posts(user_id: int, with_user_info: bool = False, db: Session = Depends(get_db)):
     if with_user_info:
-        db_user = crud.get_user_with_posts(db, user_id)
+        db_user = controllers.get_user_with_posts(db, user_id)
         if not db_user:
             raise HTTPException(status_code=404, detail="User not found")
         return db_user
-    db_user = crud.get_user_posts(db, user_id)
+    db_user = controllers.get_user_posts(db, user_id)
     if not db_user:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
@@ -43,23 +43,23 @@ def get_user_posts(user_id: int, with_user_info: bool = False, db: Session = Dep
 
 @router.post("/create", response_model=schemas.User, status_code=201)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db, user.email)
+    db_user = controllers.get_user_by_email(db, user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
-    return crud.create_user(db, user)
+    return controllers.create_user(db, user)
 
 
 @router.put("/update/{user_id}", response_model=schemas.User)
 def update_user(user: schemas.UserCreate, user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id)
+    db_user = controllers.get_user(db, user_id)
     if not db_user:
         raise HTTPException(status_code=404, detail="User not found")
-    return crud.update_user(db, user, user_id)
+    return controllers.update_user(db, user, user_id)
 
 
 @router.delete("/delete/{user_id}", status_code=204)
 def delete_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id)
+    db_user = controllers.get_user(db, user_id)
     if not db_user:
         raise HTTPException(status_code=404, detail="User not found")
-    crud.delete_user(db, user_id)
+    controllers.delete_user(db, user_id)
